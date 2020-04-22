3136e030828f20011d7f552ca4b4bce5250c4920
andresvidoza <andresvidoza@gmail.com>
2020-04-22 19:52:30 -0400

    adding new style.css sheet where I followed bootstrap guidelines towards responsive design. A lot of code is cleaned up like this

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 
-const star1 = document.querySelector('.star-1'),
-star2 = document.querySelector('.star-2'),
-star3 = document.querySelector('.star-3'),
-star4 = document.querySelector('.star-4'),
-star5 = document.querySelector('.star-5'),
-star6 = document.querySelector('.star-6'),
-star7 = document.querySelector('.star-7'),
-star8 = document.querySelector('.star-1-landscape'),
-star9 = document.querySelector('.star-2-landscape');
+// const star1 = document.querySelector('.star-1'),
+// star2 = document.querySelector('.star-2'),
+// star3 = document.querySelector('.star-3'),
+// star4 = document.querySelector('.star-4'),
+// star5 = document.querySelector('.star-5'),
+// star6 = document.querySelector('.star-6'),
+// star7 = document.querySelector('.star-7'),
+// star8 = document.querySelector('.star-1-landscape'),
+// star9 = document.querySelector('.star-2-landscape');
 
-const star1Menu = document.querySelector('.star-1-menu'),
-star2Menu = document.querySelector('.star-2-menu');
+// const star1Menu = document.querySelector('.star-1-menu'),
+// star2Menu = document.querySelector('.star-2-menu');
 
 const menuLinkList = document.querySelectorAll('.main-menu-links li a');
 //Convert node list to array
@@ -30,8 +30,8 @@ function delay (URL) {
 document.querySelector('.hamburger').addEventListener('click', () => {
     document.querySelector('.main-menu').classList.add('show');
     /* Star animation */
-    star1Menu.classList.add('animation-star-menu');
-    star2Menu.classList.add('animation-star-menu');
+    // star1Menu.classList.add('animation-star-menu');
+    // star2Menu.classList.add('animation-star-menu');
     // When menu is open, prevent from scrolling
     document.body.classList.add('fixedPosition');
 
@@ -46,8 +46,8 @@ menuLinkListArr[0].addEventListener('click', e => {
     setTimeout(function(){ 
 
         /* Star animation */
-        star1Menu.classList.remove('animation-star-menu');
-        star2Menu.classList.remove('animation-star-menu');
+        // star1Menu.classList.remove('animation-star-menu');
+        // star2Menu.classList.remove('animation-star-menu');
         // When menu is closed, allow scrolling
         document.body.classList.remove('fixedPosition');
 
@@ -61,8 +61,8 @@ menuLinkListArr[1].addEventListener('click', e => {
     setTimeout(function(){ 
 
         /* Star animation */
-        star1Menu.classList.remove('animation-star-menu');
-        star2Menu.classList.remove('animation-star-menu');
+        // star1Menu.classList.remove('animation-star-menu');
+        // star2Menu.classList.remove('animation-star-menu');
         // When menu is closed, allow scrolling
         document.body.classList.remove('fixedPosition');
 
@@ -76,8 +76,8 @@ menuLinkListArr[2].addEventListener('click', e => {
     setTimeout(function(){ 
 
         /* Star animation */
-        star1Menu.classList.remove('animation-star-menu');
-        star2Menu.classList.remove('animation-star-menu');
+        // star1Menu.classList.remove('animation-star-menu');
+        // star2Menu.classList.remove('animation-star-menu');
         // When menu is closed, allow scrolling
         document.body.classList.remove('fixedPosition');
 
@@ -91,8 +91,8 @@ hireUsBtn.addEventListener('click', e => {
     setTimeout(function(){ 
 
         /* Star animation */
-        star1Menu.classList.remove('animation-star-menu');
-        star2Menu.classList.remove('animation-star-menu');
+        // star1Menu.classList.remove('animation-star-menu');
+        // star2Menu.classList.remove('animation-star-menu');
         // When menu is closed, allow scrolling
         document.body.classList.remove('fixedPosition');
 
@@ -105,27 +105,29 @@ hireUsBtn.addEventListener('click', e => {
 document.querySelector('.hamburger-close').addEventListener('click', () => {
     document.querySelector('.main-menu').classList.remove('show');
     /* Star animation */
-    star1Menu.classList.remove('animation-star-menu');
-    star2Menu.classList.remove('animation-star-menu');
+    // star1Menu.classList.remove('animation-star-menu');
+    // star2Menu.classList.remove('animation-star-menu');
     // When menu is closed, allow scrolling
     document.body.classList.remove('fixedPosition');
 });
 
-/* Star animation for main page*/
-window.addEventListener("scroll", function() {
-    star1.style.transform = "rotate("+window.pageYOffset+"deg)";
-    star2.style.transform = "rotate("+window.pageYOffset+"deg)";
-    star3.style.transform = "rotate("+window.pageYOffset+"deg)";
-    star4.style.transform = "rotate("+window.pageYOffset+"deg)";
-    star5.style.transform = "rotate("+window.pageYOffset+"deg)";
-    star6.style.transform = "rotate("+window.pageYOffset+"deg)";
-    star7.style.transform = "rotate("+window.pageYOffset+"deg)";
-});
+// /* Star animation for main page*/
+// window.addEventListener("scroll", function() {
+//     star1.style.transform = "rotate("+window.pageYOffset+"deg)";
+//     star2.style.transform = "rotate("+window.pageYOffset+"deg)";
+//     star3.style.transform = "rotate("+window.pageYOffset+"deg)";
+//     star4.style.transform = "rotate("+window.pageYOffset+"deg)";
+//     star5.style.transform = "rotate("+window.pageYOffset+"deg)";
+//     star6.style.transform = "rotate("+window.pageYOffset+"deg)";
+//     star7.style.transform = "rotate("+window.pageYOffset+"deg)";
+// });
 
 
 /* LANDSCAPE MODE DETECTION */
 
 let width = screen.width;
+// this.document.querySelector('.result').innerHTML = width
+
 
 // check devices orientation and width when page starts
 if((window.orientation === 90 || window.orientation === -90) && width <= 500 ){
@@ -138,9 +140,9 @@ if((window.orientation === 90 || window.orientation === -90) && width <= 500 ){
     
     // Add stars
 
-    /* Star animation */
-    star8.classList.add('animation-star-menu');
-    star9.classList.add('animation-star-menu');
+    // /* Star animation */
+    // star8.classList.add('animation-star-menu');
+    // star9.classList.add('animation-star-menu');
 }else{
     //add the container, contact us and the footer
     this.document.querySelector('.container').classList.remove('landscape-mobile');
@@ -151,13 +153,14 @@ if((window.orientation === 90 || window.orientation === -90) && width <= 500 ){
 
     // Remove stars
 
-    /* Star animation */
-    star8.classList.remove('animation-star-menu');
-    star9.classList.remove('animation-star-menu');
+    // /* Star animation */
+    // star8.classList.remove('animation-star-menu');
+    // star9.classList.remove('animation-star-menu');
 }
 
 // Listen for orientation changes
 window.addEventListener("orientationchange", function() {
+    
     // Announce the new orientation number
     if((this.window.orientation === 90 || this.window.orientation === -90) && width <= 500){
         //remove the container, contact us and the footer
@@ -169,9 +172,9 @@ window.addEventListener("orientationchange", function() {
         this.document.querySelector('.landscape-mobile-sign').classList.remove('landscape-mobile');
         // Add stars
 
-        /* Star animation */
-        star8.classList.add('animation-star-menu');
-        star9.classList.add('animation-star-menu');
+        // /* Star animation */
+        // star8.classList.add('animation-star-menu');
+        // star9.classList.add('animation-star-menu');
 
     }else{
         //add the container, contact us and the footer
@@ -183,9 +186,9 @@ window.addEventListener("orientationchange", function() {
 
         // Remove stars
 
-        /* Star animation */
-        star8.classList.remove('animation-star-menu');
-        star9.classList.remove('animation-star-menu');
+        // /* Star animation */
+        // star8.classList.remove('animation-star-menu');
+        // star9.classList.remove('animation-star-menu');
 
     }
 });
